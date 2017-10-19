d3c0f44117b7327a3d05188e4262d1ddb65d9895
Alsu Abdrahmanova <aaabdra5@gmail.com>
2017-10-19 16:21:06 +0300

    Логин в админку учебного приложения

---
+++ src/test/java/TestLogin.java
@@ -0,0 +1,37 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class TestLogin {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,10);
+    }
+
+    @Test
+    public void TestLogin() {
+        driver.navigate().to("http://localhost/litecart/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("123123");
+        driver.findElement(By.name("password")).sendKeys(Keys.ENTER);
+    }
+
+    @After
+    public void stop() {
+        //driver.quit();
+        //driver = null;
+    }
+}
